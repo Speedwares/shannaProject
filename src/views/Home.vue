da0b50ad22304205ae14a23ef52c6222751737a1
overwatch <ovuobasampson@gmail.com>
2019-05-30 14:59:46 -0700

    added bootstrap vue form compnents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home">
  <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
 
       <b-form-group id="input-group-3" label="Food:" label-for="input-3">
         <b-form-select
